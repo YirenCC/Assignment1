6ac46a687f371816c633fa0accbe1f41dce9510b
YirenCC <yiren.cao@uoit.net>
2017-09-29 10:57:49 -0400

    Initial commit

---
+++ Assets/Script/Dialogue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Runtime.InteropServices;
+
+
+public class Dialogue : MonoBehaviour {
+
+    public GameObject DialogueBox;
+    public Text DialogueText;
+
+    public bool DialogueActive;
+
+    public string m_dia;
+
+    [DllImport("DialogueManager_Plugin")]
+    public static extern IntPtr LoadFile();
+
+    // Use this for initialization
+    void Start () {
+        m_dia = Marshal.PtrToStringAnsi(LoadFile());
+        Debug.Log("m_dia:" + m_dia);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(DialogueActive && Input.GetKeyDown(KeyCode.K))
+        {
+            DialogueBox.SetActive(false);
+            DialogueActive = false;
+        }
+    }
+
+    public void ShowBox()
+    {
+        DialogueActive = true;
+        DialogueBox.SetActive(true);
+        DialogueText.text = m_dia;
+    }
+}
---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    Rigidbody m_Rigidbody;
+    public float m_Speed;
+    public float m_JumpSpeed;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_Rigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_Rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * m_Speed, m_Rigidbody.velocity.y, Input.GetAxis("Vertical") * m_Speed);
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpSpeed, m_Rigidbody.velocity.z);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "DialogueZone")
+        {
+            Debug.LogWarning("Enter Region");
+            GameObject.FindObjectOfType<Dialogue>().ShowBox();
+        }
+   }
+
+}
